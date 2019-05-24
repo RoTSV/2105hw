ce2aa9b7f598c7c405ff12c72c63b28202cb0a50
pepo <pepo@ukr.net>
2019-05-24 17:28:04 +0300

    js6

--- main.js
+++ main.js
@@ -63,8 +63,11 @@ while (i <= 50) {
 }*/
 /*let arr = [1, 2, 3, 4, 5];
 for (let i = 0; i <= arr.length-1; i++) {
-	console.log(arr[i]);
+	console.log(arr.length);
 }*/
+
+
+
 //Найти произведение элементов этого массива
 /*let arr = [2, 3, 4, 5];
 let result = 1;
@@ -73,41 +76,85 @@ for (var i = 0; i < arr.length; i++) {
     console.log(result);
 }*/
 
+
+
+
+/*//Поиск max,min значения длинны массива
+
 let arr = [11, 34, 67, 4, 25];
 let max=0;
 
 for (i = 0; i <= arr.length; i++) {
-    
     if (max < arr[i]) {
         max = arr[i]; 
     }   
-    
-	
 }
-for (i = 0; i <= max; i++) {
-    
-    if (max < arr[i]) {
-        max = arr[i]; 
-    }   
-	console.log(max);
-}
+console.log(max);
+
 let min=max;
+
 for (i = 0; i <= arr.length; i++) {
-    
     if (arr[i] < min  ) {
         min = arr[i]; 
-    }   
-    
-	
+    }   	
 }
-for (i = 0; i < min; i++) {
-    
-    if (min > arr[i]) {
-        min = arr[i]; 
-    }   
-    console.log(min);
+console.log(min);*/
+
+
+
+ 
+/*
+// Преобразование строки в массив  
+let str = 'adddd вавуууу wewewewe gfgfgfgfggfgfgfgf';
+let arr = str.split([' ']);
+console.log(arr); 
+console.log(arr.length);
+*/
+/*let a=17;
+let b=10;
+let c=a-b;
+let d=7;
+
+let sum=c+d;
+console.log(sum);
+
+
+let name='Рома!';
+console.log('Привет,'+ ' '+name);*/
+/*var lang = 'ru';
+if (lang == 'ru') {
+	var arr = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
 }
-    
-    /*let str = 'adddd вавуууу wewewewe';
-    let arr = str.split([' '], [2]);
-    console.log(arr);  */
+if (lang == 'en') {
+	arr = ['mn', 'ts', 'wd', 'th', 'fr', 'st', 'sn'];
+}
+alert(arr);*/
+ 
+//Перебор массива по типу данных
+/*let arr = [11, 34, 67, 'Привет', 4, 'Hello', 25, 33, 'Пока'];
+let num = [];
+let str = [];
+    for ( i = 0; i < arr.length; i++){
+     if(typeof arr[i] === 'number'){
+         num.push(arr[i]);     
+    } else if(typeof arr[i] === 'string'){
+         str.push(arr[i]);
+    } 
+ }
+ console.log(num);
+ console.log(str);*/
+let arr = [11, 34, 67, 'Привет', 4, 'Hello', 25, 33, 'Пока', [143, '22222']];
+let num = [];
+let str = [];
+    for ( i = 0; i < arr.length; i++){
+     if(typeof arr[i] === 'number'){
+         num.push(arr[i]);     
+    } else if(typeof arr[i] === 'string'){
+         str.push(arr[i]);
+    } else if(typeof arr[i] === 'object'){
+        num.push(arr[i]);
+    }
+ }
+ console.log(num);
+ console.log(str);
+ 
